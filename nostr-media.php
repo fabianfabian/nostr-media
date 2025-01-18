f87fd7ea7d5b4bf4b8e1c2edfce793900ff6a394
Fabian <fabian@nostur.com>
2025-01-18 15:12:58 +0100

    Bump version

--- nostr-media.php
+++ nostr-media.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Nostr Media Uploads
  * Description: Host the images you post on nostr on your own WordPress installation
- * Version: 0.10
+ * Version: 0.11
  * Author: Fabian Lachman
  */
 
